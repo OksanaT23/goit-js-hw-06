00ecb1af54912d378dd3413185864d73750df45f
OksanaT23 <129532941+OksanaT23@users.noreply.github.com>
2023-08-28 01:34:43 +0300

    Homework 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('form.login-form');
 
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const {
+        email: { name: emailName, value: emailValue },
+        password: { name: passwordName, value: passwordValue }
+    } = form.elements;
+
+    if (!emailValue.length || !passwordValue.length) {
+        alert('Bсі поля повинні бути заповнені.');
+
+        return;
+    }
+
+    const formData = {
+        [emailName]: emailValue,
+        [passwordName]: passwordValue,
+    };
+
+    console.log(formData);
+    
+    form.reset();
+});
