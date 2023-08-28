40467656f8a757f0773f2370f2206781d4b2df61
OksanaT23 <129532941+OksanaT23@users.noreply.github.com>
2023-08-28 19:40:41 +0300

    Homework 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const amountInput = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  let boxesHtml = '';
+
+  for (let i = 0; i < amount; i++) {
+    const color = getRandomHexColor();
+    const size = 30 + i * 10;
+
+    boxesHtml += `<div style="width: ${size}px; height:${size}px; background-color:${color}"></div>`;
+  }
+
+  boxesContainer.innerHTML = boxesHtml;
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createBtn.addEventListener('click', event => {
+  const amount = amountInput.value;
+  createBoxes(amount);
+});
+destroyBtn.addEventListener('click', destroyBoxes);
